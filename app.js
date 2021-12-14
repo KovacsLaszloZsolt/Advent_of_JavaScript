143989d1b907c9233c91f4738bf6d4a62bfb619d
Kovacs Laszlo Zsolt <laszlo.zs.kovacs@gmail.com>
2021-12-14 22:22:06 +0000

    done task

---
+++ app.js
@@ -0,0 +1,27 @@
+const inputFields = [...document.querySelector(".fields").children];
+const codeLength = inputFields.length;
+inputFields.forEach((elem, index) => {
+  elem.addEventListener("input", (e) => {
+    if (e.target.value.length === 1 && index < codeLength - 1) {
+      inputFields[index + 1].focus();
+    }
+  });
+
+  elem.addEventListener("paste", (e) => {
+    const pastedText = e.clipboardData
+      .getData("text/plain")
+      .slice(0, codeLength)
+      .split("");
+
+    pastedText.forEach((char, index) => {
+      inputFields[index].value = char;
+    });
+    inputFields[codeLength - 1].focus();
+  });
+
+  //   inputFields[0].addEventListener("paste", (e) => {
+  //     e.stopPropagation();
+  //     e.preventDefault();
+  //     console.log(e.clipboardData.getData("text/plain"));
+  //   });
+});
